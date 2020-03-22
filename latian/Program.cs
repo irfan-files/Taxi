3e45d23d58c6de42e5555fb13e9eb0373356816b
irfan <irfan.maulana@students.amikom.ac.id>
2020-03-22 21:12:29 +0700

    Add project files.

---
+++ latian/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace latian
+{
+    class Taxi
+    {
+        string DriverName;
+        bool OnDuty;
+        int NumPassanger;
+
+        static void Main(string[] args)
+        {
+            Taxi taxi = new Taxi();
+            taxi.DriverName = "Jono";
+            taxi.OnDuty = true;
+            taxi.NumPassanger = 10;
+
+            
+            taxi.TaxiInfo();
+            taxi.PickUpPassanger();
+            taxi.DropOffPassanger();
+
+            Console.WriteLine(taxi.DriverName);
+
+            /*Console.WriteLine(Opel.model);*/
+        }
+        public void TaxiInfo()
+        {
+            Console.WriteLine("Driver Name : " + this.DriverName);
+            if (OnDuty == true)
+            {
+                Console.WriteLine("On Duty : yes");
+            }
+            else if (OnDuty == false)
+            {
+                Console.WriteLine("On Duty : no");
+            }
+            Console.WriteLine("Number Of Passenger : " + this.NumPassanger);
+        }
+
+        public void PickUpPassanger()
+        {
+            Console.WriteLine(this.DriverName + " sedang menjemput penumpang");
+        }
+
+        public void DropOffPassanger()
+        {
+            Console.Write(this.DriverName + " selesai mengantar penumpang");
+        }
+
+    }
+
+}
+
